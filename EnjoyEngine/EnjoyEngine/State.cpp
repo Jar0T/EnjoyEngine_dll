0e7b188a66b9dc4ce73c3303a5c0a050e382e7e6
Jar0T <jarotumula4@gmail.com>
2020-04-27 15:39:09 +0200

    Added State destructor implementation

---
+++ EnjoyEngine/EnjoyEngine/State.cpp
@@ -0,0 +1,11 @@
+// Precompiled headers
+#include "pch.h"
+
+// System headers
+
+// My headers
+#include "State.hpp"
+
+namespace EE {
+	State::~State() {}
+}
--- EnjoyEngine/EnjoyEngine/State.hpp
+++ EnjoyEngine/EnjoyEngine/State.hpp
@@ -6,7 +6,7 @@ namespace EE {
 
 	public:
 		State() {};
-		virtual ~State() = 0;
+		virtual ~State();
 
 		virtual void update() = 0;
 		virtual void handleEvents() = 0;
