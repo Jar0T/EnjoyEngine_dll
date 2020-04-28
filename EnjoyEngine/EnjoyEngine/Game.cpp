ea1e7cf2dfb12abb5a4d63fb38d621a91aa43168
Jar0T <jarotumula4@gmail.com>
2020-04-28 15:26:05 +0200

    Game loop uses state machine

--- EnjoyEngine/EnjoyEngine/Game.cpp
+++ EnjoyEngine/EnjoyEngine/Game.cpp
@@ -6,6 +6,9 @@
 // My headers
 #include "Game.hpp"
 #include "Time.hpp"
+#include "GameData.hpp"
+#include "StateMachine.hpp"
+#include "State.hpp"
 
 namespace EE {
 	int Game::Init() {
@@ -16,12 +19,15 @@ namespace EE {
 
 	int Game::Play() {
 		float lastUpdate = 0.f;
-		while (1) {
+		while (GameData::window().isOpen()) {
 			Time::setDeltaTime(this->_clock.getElapsedTime().asSeconds() - lastUpdate);
 			lastUpdate = this->_clock.getElapsedTime().asSeconds();
 
-			// update
-			// handle events
+			std::shared_ptr<State> state = StateMachine::getActiveState();
+			if (state != nullptr) {
+				state->update();
+				state->handleEvents();
+			}
 		}
 
 		return 0;
