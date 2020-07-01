3e37223a7abf4030e7de9358fbfb68729df691f4
Jar0T <jarotumula4@gmail.com>
2020-07-01 17:43:04 +0200

    Added AABB collider component for 2D objects

---
+++ EnjoyEngine/EnjoyEngine/AABBCollider2DComponent.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "Component.hpp"
+#include "Vector2D.hpp"
+
+#ifdef ENJOYENGINE_EXPORTS
+#define ENJOYENGINE_API __declspec(dllexport)
+#else
+#define ENJOYENGINE_API __declspec(dllimport)
+#endif
+
+namespace EE {
+	class ENJOYENGINE_API AABBCollider2DComponent : public Component {
+	private:
+		Vector2D<float> _position;
+		Vector2D<float> _size;
+
+	public:
+		AABBCollider2DComponent();
+		~AABBCollider2DComponent();
+
+		Vector2D<float>& position();
+		Vector2D<float>& size();
+
+	};
+}
