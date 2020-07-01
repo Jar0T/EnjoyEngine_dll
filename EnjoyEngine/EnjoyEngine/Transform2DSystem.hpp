b491826a565f2dc05c2da1c9883f46f151b1c5eb
Jar0T <jarotumula4@gmail.com>
2020-07-01 19:08:56 +0200

    Moved some includes to System.hpp file

--- EnjoyEngine/EnjoyEngine/Transform2DSystem.hpp
+++ EnjoyEngine/EnjoyEngine/Transform2DSystem.hpp
@@ -2,9 +2,6 @@
 
 #include "System.hpp"
 #include "Transform2DComponent.hpp"
-#include "Time.hpp"
-#include "EntityManager.hpp"
-#include "ComponentManager.hpp"
 
 #ifdef ENJOYENGINE_EXPORTS
 #define ENJOYENGINE_API __declspec(dllexport)
@@ -14,8 +11,6 @@
 
 namespace EE {
 	class ENJOYENGINE_API Transform2DSystem : public System {
-	private:
-
 	public:
 		Transform2DSystem();
 		~Transform2DSystem();
