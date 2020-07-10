50da8beef77b34452097504419e343199fb91524
Jar0T <jarotumula4@gmail.com>
2020-07-10 10:40:46 +0200

    Added implementation for some ecs methods

--- EnjoyEngine/EnjoyEngine/ecs.cpp
+++ EnjoyEngine/EnjoyEngine/ecs.cpp
@@ -18,7 +18,39 @@ namespace EE {
 	ECS::ECS() {}
 
 	ECS::~ECS() {
-		
+		for (std::map<std::uint32_t, std::vector<std::uint8_t>>::iterator it = _components.begin(); it != _components.end(); it++) {
+			size_t typeSize = BaseECSComponent::getTypeSize(it->first);
+			ECSComponentFreeFunction freefn = BaseECSComponent::getTypeFreeFunction(it->first);
+
+			for (std::uint32_t i = 0; i < it->second.size(); i += typeSize) {
+				freefn((BaseECSComponent*)&it->second[i]);
+			}
+		}
+
+		for (std::uint32_t i = 0; i < _entities.size(); i++) {
+			delete _entities[i];
+		}
+	}
+
+	EntityHandle ECS::createEntity(BaseECSComponent* components, const std::uint32_t* componentIDs, size_t noComponents) {
+		std::pair<std::uint32_t, std::vector<std::pair<std::uint32_t, std::uint32_t>>>* newEntity =
+			new std::pair<std::uint32_t, std::vector<std::pair<std::uint32_t, std::uint32_t>>>();
+		EntityHandle handle = (EntityHandle)newEntity;
+
+		for (size_t i = 0; i < noComponents; i++) {
+			// Check if component's ID is valid
+
+			ECSComponentCreateFunction createfn = BaseECSComponent::getTypeCreateFunction(componentIDs[i]);
+			std::pair<std::uint32_t, std::uint32_t> newPair;
+			newPair.first = componentIDs[i];
+			newPair.second = createfn(_components[componentIDs[i]], handle, &components[i]);
+			newEntity->second.push_back(newPair);
+		}
+
+		newEntity->first = _entities.size();
+		_entities.push_back(newEntity);
+
+		return handle;
 	}
 
 	inline void ECS::addSystem(BaseECSSystem& system) {
