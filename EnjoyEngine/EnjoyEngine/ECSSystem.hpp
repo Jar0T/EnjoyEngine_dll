bfb243d414064890452b12e2a27fe1db2a80f553
Jar0T <jarotumula4@gmail.com>
2020-07-16 12:53:06 +0200

    Moved system implementation to cpp file

--- EnjoyEngine/EnjoyEngine/ECSSystem.hpp
+++ EnjoyEngine/EnjoyEngine/ECSSystem.hpp
@@ -16,27 +16,16 @@ namespace EE {
 		std::vector<std::uint32_t> _componetnFlags;
 
 	protected:
-		void addComponentType(uint32_t componetnType, Flags componentFlag = Flags::NO_FLAG) {
-			_componentTypes.push_back(componetnType);
-			_componetnFlags.push_back((uint32_t)componentFlag);
-		}
+		void addComponentType(uint32_t componetnType, Flags componentFlag = Flags::NO_FLAG);
 
 	public:
-		BaseECSSystem() {};
+		BaseECSSystem();
 
-		virtual void updateComponents(BaseECSComponent** components) {};
+		virtual void updateComponents(BaseECSComponent** components);
 
-		const std::vector<std::uint32_t>& getComponentTypes() { return _componentTypes; };
-		const std::vector<uint32_t>& getComponentFlags() { return _componetnFlags; };
+		const std::vector<std::uint32_t>& getComponentTypes();
+		const std::vector<uint32_t>& getComponentFlags();
 
-		bool isValid() {
-			for (size_t i = 0; i < _componetnFlags.size(); i++) {
-				if ((_componetnFlags[i] & (uint32_t)BaseECSSystem::Flags::OPTIONAL_COMPONENT) == 0) {
-					return true;
-				}
-			}
-			return false;
-		}
-
+		bool isValid();
 	};
 }
