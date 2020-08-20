2f108aa1ddb182d9133cad0ea7b4378f62373260
Jar0T <jarotumula4@gmail.com>
2020-08-20 11:29:31 +0200

    Added includes to main engine include file

--- EnjoyEngine/EnjoyEngine/EnjoyEngine.hpp
+++ EnjoyEngine/EnjoyEngine/EnjoyEngine.hpp
@@ -11,6 +11,7 @@
 #pragma endregion
 
 #pragma region managers
+#include "SoundManager.hpp"
 #include "TextureManager.hpp"
 #include "InputManager.hpp"
 #pragma endregion
@@ -22,4 +23,15 @@
 
 #pragma region time
 #include "Time.hpp"
+#pragma endregion
+
+#pragma region ecs
+#include "ECSComponent.hpp"
+#include "ECSSystem.hpp"
+#include "ecs.hpp"
+#pragma endregion
+
+#pragma region game_object
+#include "GameObject.hpp"
+#include "Camera.hpp"
 #pragma endregion
